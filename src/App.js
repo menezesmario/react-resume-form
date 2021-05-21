fcd388f0f37f073085e5b01c1bb489cac9d17a36
Mário Menezes <mario082@gmail.com>
2021-05-21 13:35:04 -0300

    first commit basic form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import React from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+
+const schema = yup.object().shape({
+  name: yup.string().required(),
+  email: yup.string().email().required(),
+  phone: yup.number().required(),
+  address: yup.string().required(),
+  curriculum: yup.mixed().required().test("fileSize", "O arquivo deve ter no máximo 500kb", (value) => {
+    return value && value[0].size <= 500000
+  }),
+
+});
+
+export default function App() {
+  const { register, handleSubmit, formState:{ errors } } = useForm({
+    resolver: yupResolver(schema)
+  });
+  const onSubmit = data => console.log(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>Nome</label>
+      <input type="text" name="candidate_name" {...register("name")} />
+      <p>{errors.name?.message}</p>
+
+      <label>Email</label>  
+      <input type="email" name="email" {...register("email")} />
+      <p>{errors.email?.message}</p>
+
+      <label>Telefone</label>  
+      <input type="number" name="phone" {...register("phone")} />
+      <p>{errors.phone?.message}</p>
+
+      <label>Endereço</label>  
+      <input type="text" name="address" {...register("address")} />
+      <p>{errors.address?.message}</p>
+
+      <label>Currículo</label>  
+      <input ref={register} required type="file" name="curriculum" {...register("curriculum")} />
+      <p>{errors.curriculum?.message}</p>
+      
+      <input type="submit" />
+    </form>
   );
 }
-
-export default App;
